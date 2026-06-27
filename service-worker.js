5e493afcb332f8f73995f4119bf3953c66bd7fe9
DEDDDEE <153159731+DEDDDEE@users.noreply.github.com>
2026-06-28 05:23:59 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@ const files = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(files)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
@@ -18,9 +19,15 @@ self.addEventListener("activate", (event) => {
       Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request).catch(() => caches.match("./index.html")));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
